5f3da0a21d171e75361fdfbfbd5be3b87d4c56ad
Dirk Mahler <dirk.mahler@buschmais.com>
2020-10-13 14:08:18 +0200

    fixed SQ issue

--- src/test/java/org/jqassistant/contrib/sonarqube/plugin/sensor/JQAssistantSensorTest.java
+++ src/test/java/org/jqassistant/contrib/sonarqube/plugin/sensor/JQAssistantSensorTest.java
@@ -5,6 +5,8 @@ import org.jqassistant.schema.report.v1.ExecutableRuleType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.sonar.api.batch.fs.FileSystem;
@@ -40,46 +42,26 @@ public class JQAssistantSensorTest {
     @BeforeEach
     public void setUp() throws URISyntaxException {
         baseDir = new File(JQAssistantSensorTest.class.getResource("/").toURI().getPath());
+        sensor = new JQAssistantSensor(configuration, issueHandler);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "jqassistant-report-concept-issue.xml", "jqassistant-report-1_8.xml", "jqassistant-report-constraint-issue.xml" })
+    public void issues(String reportWithIssue) {
+        stubFileSystem(reportWithIssue);
+
+        sensor.execute(sensorContext);
+
+        verify(issueHandler).process(eq(sensorContext), any(File.class), any(ExecutableRuleType.class));
     }
 
     @Test
     public void noIssue() {
-        sensor = new JQAssistantSensor(configuration, issueHandler);
         stubFileSystem("jqassistant-report-no-issue.xml");
 
         sensor.execute(sensorContext);
 
         verify(issueHandler, never()).process(eq(sensorContext), any(File.class), any(ExecutableRuleType.class));
-    }
-
-    @Test
-    public void createConceptIssue() {
-        sensor = new JQAssistantSensor(configuration, issueHandler);
-        stubFileSystem("jqassistant-report-concept-issue.xml");
-
-        sensor.execute(sensorContext);
-
-        verify(issueHandler).process(eq(sensorContext), any(File.class), any(ExecutableRuleType.class));
-    }
-
-    @Test
-    public void createConceptIssue_1_8() {
-        sensor = new JQAssistantSensor(configuration, issueHandler);
-        stubFileSystem("jqassistant-report-1_8.xml");
-
-        sensor.execute(sensorContext);
-
-        verify(issueHandler).process(eq(sensorContext), any(File.class), any(ExecutableRuleType.class));
-    }
-
-    @Test
-    public void createConstraintIssue() {
-        sensor = new JQAssistantSensor(configuration, issueHandler);
-        stubFileSystem("jqassistant-report-constraint-issue.xml");
-
-        sensor.execute(sensorContext);
-
-        verify(issueHandler).process(eq(sensorContext), any(File.class), any(ExecutableRuleType.class));
     }
 
     private void stubFileSystem(String reportFile) {
