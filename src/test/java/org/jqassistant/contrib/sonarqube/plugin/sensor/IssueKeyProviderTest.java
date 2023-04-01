cea95530ffc74c3d68b6089c5cf9b3d2e62b7a52
Dirk Mahler <dirk.mahler@buschmais.com>
2023-04-01 16:50:30 +0200

    added IssueKeyProviderTest

---
+++ src/test/java/org/jqassistant/contrib/sonarqube/plugin/sensor/IssueKeyProviderTest.java
@@ -0,0 +1,70 @@
+package org.jqassistant.contrib.sonarqube.plugin.sensor;
+
+import org.jqassistant.schema.report.v2.ColumnType;
+import org.jqassistant.schema.report.v2.ExecutableRuleType;
+import org.jqassistant.schema.report.v2.RowType;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static java.util.Collections.singletonList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.jqassistant.contrib.sonarqube.plugin.sensor.RuleType.CONCEPT;
+import static org.jqassistant.contrib.sonarqube.plugin.sensor.RuleType.CONSTRAINT;
+import static org.mockito.Mockito.*;
+
+/**
+ * Verifies the {@link IssueKeyProvider}.
+ */
+@ExtendWith(MockitoExtension.class)
+class IssueKeyProviderTest {
+
+    private final IssueKeyProvider issueKeyProvider = new IssueKeyProvider();
+
+    @Mock
+    private ExecutableRuleType executableRuleType;
+
+    @Test
+    void conceptIssueKey() {
+        doReturn("test:Concept").when(executableRuleType)
+            .getId();
+
+        String issueKey = issueKeyProvider.getIssueKey(executableRuleType, CONCEPT);
+
+        assertThat(issueKey).hasSize(64);
+        verify(executableRuleType).getId();
+    }
+
+    @Test
+    void constraintIssueKeyWithRowKey() {
+        RowType rowType = mock(RowType.class);
+        doReturn("1").when(rowType)
+            .getKey();
+
+        String issueKey = issueKeyProvider.getIssueKey(executableRuleType, rowType, CONSTRAINT);
+
+        assertThat(issueKey).isEqualTo("1");
+        verify(executableRuleType, never()).getId();
+        verify(rowType).getKey();
+    }
+
+    @Test
+    void constraintIssueKeyWithoutRowKey() {
+        doReturn("test:Constraint").when(executableRuleType)
+            .getId();
+        RowType rowType = mock(RowType.class);
+        ColumnType columnType = mock(ColumnType.class);
+        doReturn(singletonList(columnType)).when(rowType)
+            .getColumn();
+        doReturn("value").when(columnType)
+            .getValue();
+
+        String issueKey = issueKeyProvider.getIssueKey(executableRuleType, rowType, CONSTRAINT);
+
+        assertThat(issueKey).hasSize(64);
+        verify(rowType).getKey();
+        verify(executableRuleType).getId();
+        verify(columnType).getValue();
+    }
+}
